ac98a7af8d5c3d42a6dd3ba137905edab03f846d
danilodamasceno <danilo.g.d@hotmail.com>
2015-03-26 16:33:26 -0300

    menu em php
    foi criado um me nu lateral gerado por php

---
+++ Sistemas WEB I/Conteudos/formulario/verifica.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+<?php  
+
+echo $_POST["nome"];
+echo $_POST["sobrenome"];
+echo $_POST["interesse"];
+echo $_POST["email"];
+echo $_POST["gender"]; 
+
+
+
+
+?>
+
+
+</body>
+</html>
+
+
+
---
+++ Sistemas WEB I/Conteudos/menuLateral.php
@@ -0,0 +1,14 @@
+
+<ul>
+<li><a href="quem.html"> <h2><i> Quem somos <img src="interrog.jpg" height="20" width="20">  </h2> </i> </a>  </li> 
+
+<li><a href="oferecemos.html"><h2><i>   O que oferecemos <img src="interrog.jpg" height="20" width="20"><a/> </i> </h2> </li>
+
+<li><a href="web.html"> <h2><i> O que é Web <img src="interrog.jpg" height="20" width="20"><a/> </i> </h2> </li>
+
+<li><a href="tecnologias.html"> <h2> <i> Tecnologias <img src="tec.jpg" height="50" width="80">  </h2>  </i> </a> </li>
+
+<li><a href="metodos.html"> <h2> <i> Métodos <img src="met.jpg" height="70" width="70">  </h2> </i> </a> </li>
+
+</ul>
+
